bdd817c8889b44ef634c65ac896c4f9f5607c425
zheng_xin_quan <zheng_xin_quan@163.com>
2023-05-12 15:09:55 +0800

    style

--- src/App.vue
+++ src/App.vue
@@ -709,14 +709,3 @@ export default {
   mixins: [pageMiXin],
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
