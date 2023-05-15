df194d1cdb87f888ad942b4d89e5ee1e50319433
zheng_xin_quan <zheng_xin_quan@163.com>
2023-05-15 11:01:29 +0800

    return client

--- api/class/utils.js
+++ api/class/utils.js
@@ -4,7 +4,7 @@ const uri =
   'mongodb+srv://zhengxinquan:hqmMkpDhJi9Zwfa8@blog-c.iy6i7qc.mongodb.net/?retryWrites=true&w=majority';
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = () => {
-  new MongoClient(uri, {
+  return new MongoClient(uri, {
     serverApi: {
       version: ServerApiVersion.v1,
       strict: true,
