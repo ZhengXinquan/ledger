fc04baceb750554da043c2a789559fb519451067
zheng_xin_quan <zheng_xin_quan@163.com>
2023-06-05 16:47:13 +0800

    fix insert income failed

--- api/class/income.js
+++ api/class/income.js
@@ -1,27 +1,27 @@
-const MODULE_NAME = 'aa_income'
-const { RES, client, ObjectId, moment } = require('./utils')
+const MODULE_NAME = 'aa_income';
+const { RES, client, ObjectId, moment } = require('./utils');
 class Clasbname {
   constructor(TOKEN_USER_INFO) {
-    this.DATABASE = client().db('hdm189315162_db')
-    this.COLLECTION = this.DATABASE.collection(MODULE_NAME)
+    this.DATABASE = client().db('hdm189315162_db');
+    this.COLLECTION = this.DATABASE.collection(MODULE_NAME);
   }
 
   delete(id_) {
     return new Promise(async (resolve, reject) => {
       try {
         let conditions = {
-          _id: new ObjectId(id_)
-        }
+          _id: new ObjectId(id_),
+        };
 
-        const result = await this.COLLECTION.deleteOne(conditions)
-        console.log(result, conditions)
+        const result = await this.COLLECTION.deleteOne(conditions);
+        console.log(result, conditions);
 
-        resolve(RES.success(MODULE_NAME + 'Succeed DELETE'))
+        resolve(RES.success(MODULE_NAME + 'Succeed DELETE'));
       } finally {
-        resolve(RES.error(MODULE_NAME + 'DELETE failed'))
+        resolve(RES.error(MODULE_NAME + 'DELETE failed'));
         // await client.close()
       }
-    })
+    });
   }
   insert(bid_, iname_, imoney_, iday_) {
     return new Promise(async (resolve, reject) => {
@@ -31,65 +31,65 @@ class Clasbname {
           iname: iname_,
           imoney: Number(imoney_),
           iday: iday_,
-          _bid:new ObjectId(bid_)
+          _bid: new ObjectId(bid_),
           _iday: new Date(iday_),
-          _itime: new Date()
-        }
-        const result = await this.COLLECTION.insertOne(doc)
-        console.log('result')
-        console.log(result)
+          _itime: new Date(),
+        };
+        const result = await this.COLLECTION.insertOne(doc);
+        console.log('result');
+        console.log(result);
 
-        resolve(RES.success(MODULE_NAME + ' Succeed insert'))
+        resolve(RES.success(MODULE_NAME + ' Succeed insert'));
       } finally {
-        resolve(RES.error(MODULE_NAME + ' Insert failed'))
+        resolve(RES.error(MODULE_NAME + ' Insert failed'));
         // await client.close()
       }
-    })
+    });
   }
   updateType(id_, bid_) {
     return new Promise(async (resolve, reject) => {
       try {
-        const whereStr = { _id: new ObjectId(id_) }
+        const whereStr = { _id: new ObjectId(id_) };
         const updateStr = {
           $set: {
             bid: bid_,
-            _bid:new ObjectId(bid_)
-          }
-        }
-        const result = await this.COLLECTION.updateOne(whereStr, updateStr)
-        console.log('result')
-        console.log(result)
+            _bid: new ObjectId(bid_),
+          },
+        };
+        const result = await this.COLLECTION.updateOne(whereStr, updateStr);
+        console.log('result');
+        console.log(result);
 
-        resolve(RES.success(MODULE_NAME + ' Succeed updateType '))
+        resolve(RES.success(MODULE_NAME + ' Succeed updateType '));
       } finally {
-        resolve(RES.error(MODULE_NAME + ' updateType failed'))
+        resolve(RES.error(MODULE_NAME + ' updateType failed'));
         // await client.close()
       }
-    })
+    });
   }
-  updateDetail(id_, iname_,imoney_,iday_) {
+  updateDetail(id_, iname_, imoney_, iday_) {
     return new Promise(async (resolve, reject) => {
       try {
-        const whereStr = { _id: new ObjectId(id_) }
+        const whereStr = { _id: new ObjectId(id_) };
         const updateStr = {
           $set: {
-            iname:iname_,
+            iname: iname_,
             imoney: Number(imoney_),
-            _iday:new Date(iday_),
-            iday:iday_
-          }
-        }
-        const result = await this.COLLECTION.updateOne(whereStr, updateStr)
-        console.log('result')
-        console.log(result)
+            _iday: new Date(iday_),
+            iday: iday_,
+          },
+        };
+        const result = await this.COLLECTION.updateOne(whereStr, updateStr);
+        console.log('result');
+        console.log(result);
 
-        resolve(RES.success(MODULE_NAME + ' Succeed updateDetail '))
+        resolve(RES.success(MODULE_NAME + ' Succeed updateDetail '));
       } finally {
-        resolve(RES.error(MODULE_NAME + ' updateDetail failed'))
+        resolve(RES.error(MODULE_NAME + ' updateDetail failed'));
         // await client.close()
       }
-    })
+    });
   }
 }
 
-module.exports = Clasbname
+module.exports = Clasbname;
